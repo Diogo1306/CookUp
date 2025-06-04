d2dee83040b98f546287e31b3139e4b3ffc7a0bf
DiogoEsteves <diogoestevesmorgado@gmail.com>
2025-06-04 17:39:55 +0100

    new layouts for almost everything that is missing, adjustments to some things

--- app/src/main/java/com/diogo/cookup/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/diogo/cookup/ui/fragment/ProfileFragment.java
@@ -1,11 +1,11 @@
 package com.diogo.cookup.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -28,11 +28,17 @@ public class ProfileFragment extends Fragment {
     }
     
     private void setupListeners(View view) {
-        Button btnSettings = view.findViewById(R.id.btn_settings);
+        ImageButton btnSettings = view.findViewById(R.id.btn_settings);
+        ImageButton btnEditProfile = view.findViewById(R.id.btn_editprofile);
 
         btnSettings.setOnClickListener(v -> openSettingsFragment());
+        btnEditProfile.setOnClickListener( v ->openProfileSettingsFragment());
     }
 
+    private void openProfileSettingsFragment() {
+        NavHostFragment.findNavController(this)
+                .navigate(R.id.action_profileFragment_to_settingsProfileFragment);
+    }
     private void openSettingsFragment() {
         NavHostFragment.findNavController(this)
                 .navigate(R.id.action_profileFragment_to_settingsFragment);
