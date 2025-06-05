3c51fcafe3b45eaa6c7cad24d62e37bc106dd72b
DiogoEsteves <diogoestevesmorgado@gmail.com>
2025-06-05 15:23:43 +0100

    profile info on app, add author recipes and need more function to get prefect

---
+++ app/app/src/main/java/com/diogo/cookup/ui/adapter/ProfileRecipeAdapter.java
@@ -0,0 +1,96 @@
+package com.diogo.cookup.ui.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.diogo.cookup.R;
+import com.diogo.cookup.data.model.RecipeData;
+
+import java.util.List;
+
+public class ProfileRecipeAdapter extends RecyclerView.Adapter<ProfileRecipeAdapter.ProfileRecipeViewHolder> {
+
+    public interface OnRecipeActionListener {
+        void onEdit(RecipeData recipe);
+        void onDelete(RecipeData recipe);
+        void onRecipeClick(RecipeData recipe);
+    }
+
+    private List<RecipeData> recipeList;
+    private final OnRecipeActionListener actionListener;
+    private final Context context;
+
+    public ProfileRecipeAdapter(Context context, List<RecipeData> recipes, OnRecipeActionListener listener) {
+        this.context = context;
+        this.recipeList = recipes;
+        this.actionListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ProfileRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_recipe_user_management, parent, false);
+        return new ProfileRecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileRecipeViewHolder holder, int position) {
+        RecipeData recipe = recipeList.get(position);
+
+        holder.recipeTitle.setText(recipe.getTitle());
+        holder.recipeViews.setText(String.valueOf(recipe.getViewsCount()));
+        holder.ratingText.setText(String.format("%.1f", recipe.getAverageRating()));
+
+        Glide.with(context)
+                .load(recipe.getImage())
+                .placeholder(R.drawable.placeholder)
+                .into(holder.recipeImage);
+
+        holder.buttonEdit.setOnClickListener(v -> {
+            if (actionListener != null) actionListener.onEdit(recipe);
+        });
+        holder.buttonDelete.setOnClickListener(v -> {
+            if (actionListener != null) actionListener.onDelete(recipe);
+        });
+        holder.itemView.setOnClickListener(v -> {
+            if (actionListener != null) actionListener.onRecipeClick(recipe);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeList != null ? recipeList.size() : 0;
+    }
+
+    public void setRecipeList(List<RecipeData> newList) {
+        Log.d("ProfileRecipeAdapter", "Receitas recebidas: " + newList.size());
+        this.recipeList = newList;
+        notifyDataSetChanged();
+    }
+
+    static class ProfileRecipeViewHolder extends RecyclerView.ViewHolder {
+        ImageView recipeImage;
+        TextView recipeTitle, recipeViews, ratingText;
+        View buttonEdit, buttonDelete;
+
+        public ProfileRecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeImage = itemView.findViewById(R.id.recipe_image);
+            recipeTitle = itemView.findViewById(R.id.recipe_title);
+            recipeViews = itemView.findViewById(R.id.recipe_views);
+            ratingText = itemView.findViewById(R.id.rating_text);
+            buttonEdit = itemView.findViewById(R.id.button_edit_recipe);
+            buttonDelete = itemView.findViewById(R.id.button_delete_recipe);
+        }
+    }
+}
--- app/app/src/main/java/com/diogo/cookup/ui/fragment/ProfileFragment.java
+++ app/app/src/main/java/com/diogo/cookup/ui/fragment/ProfileFragment.java
@@ -1,44 +1,153 @@
 package com.diogo.cookup.ui.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.diogo.cookup.R;
-import com.diogo.cookup.viewmodel.AuthViewModel;
+import com.diogo.cookup.data.model.RecipeData;
+import com.diogo.cookup.data.model.UserData;
+import com.diogo.cookup.ui.adapter.ProfileRecipeAdapter;
+import com.diogo.cookup.utils.SharedPrefHelper;
+import com.diogo.cookup.viewmodel.ProfileViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
 
-    private AuthViewModel authViewModel;
+    private ProfileViewModel profileViewModel;
+    private ProfileRecipeAdapter adapter;
+    ImageView profile_image;
+    TextView user_name, user_rating, user_total_views, total_finished_my_recipes, total_recipes;
+    RecyclerView UserRecipesRecyclerView;
+    FloatingActionButton fab_add_recipe;
+    ImageButton btnSettings, btnEditProfile;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+
+        profileViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
+
+        UserData user = SharedPrefHelper.getInstance(requireContext()).getUser();
+        if (user != null) {
+            profileViewModel.loadProfileSummary(user.getUserId());
+            profileViewModel.loadProfileRecipes(user.getUserId());
+        }
+
+        setupViews(view);
         setupListeners(view);
+        loadUserFromLocalStorage();
+        observeViewModels();
+
         return view;
     }
-    
-    private void setupListeners(View view) {
-        ImageButton btnSettings = view.findViewById(R.id.btn_settings);
-        ImageButton btnEditProfile = view.findViewById(R.id.btn_editprofile);
 
+    private void setupViews(View view) {
+        btnSettings = view.findViewById(R.id.btn_settings);
+        btnEditProfile = view.findViewById(R.id.btn_editprofile);
+        profile_image = view.findViewById(R.id.profile_image);
+        user_name = view.findViewById(R.id.user_name);
+        user_rating = view.findViewById(R.id.user_rating);
+        user_total_views = view.findViewById(R.id.user_total_views);
+        total_finished_my_recipes = view.findViewById(R.id.total_finished_my_recipes);
+        UserRecipesRecyclerView = view.findViewById(R.id.UserRecipesRecyclerView);
+        fab_add_recipe = view.findViewById(R.id.fab_add_recipe);
+        total_recipes = view.findViewById(R.id.total_recipes);
+    }
+
+    private void setupListeners(View view) {
         btnSettings.setOnClickListener(v -> openSettingsFragment());
-        btnEditProfile.setOnClickListener( v ->openProfileSettingsFragment());
+        btnEditProfile.setOnClickListener(v -> openProfileSettingsFragment());
+        fab_add_recipe.setOnClickListener(v -> openSaveRecipesFragment());
+
+        UserRecipesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new ProfileRecipeAdapter(
+                getContext(),
+                new ArrayList<>(),
+                new ProfileRecipeAdapter.OnRecipeActionListener() {
+                    @Override
+                    public void onEdit(RecipeData recipe) {
+                        // TODO: abrir tela de edição (ou BottomSheet)
+                    }
+
+                    @Override
+                    public void onDelete(RecipeData recipe) {
+                        // TODO: mostrar dialog de confirmação e deletar
+                    }
+
+                    @Override
+                    public void onRecipeClick(RecipeData recipe) {
+                        // TODO: abrir detalhe da receita
+                    }
+                }
+        );
+        UserRecipesRecyclerView.setAdapter(adapter);
+
+    }
+
+    private void loadUserFromLocalStorage() {
+        UserData user = SharedPrefHelper.getInstance(requireContext()).getUser();
+        if (user != null) {
+            user_name.setText(user.getUsername());
+
+            if (user.getProfilePicture() != null && !user.getProfilePicture().isEmpty()) {
+                Glide.with(this)
+                        .load(user.getProfilePicture())
+                        .placeholder(R.drawable.placeholder)
+                        .into(profile_image);
+            }
+        }
+    }
+
+
+    private void observeViewModels() {
+
+        profileViewModel.getProfileSummary().observe(getViewLifecycleOwner(), profileData -> {
+            if (profileData != null) {
+                user_rating.setText(String.valueOf(profileData.getAverageRating()));
+                user_total_views.setText(String.valueOf(profileData.getTotalViews()));
+                total_finished_my_recipes.setText(String.valueOf(profileData.getFinishedCount()));
+                TextView totalRecipesTextView = requireView().findViewById(R.id.total_recipes);
+                totalRecipesTextView.setText(String.valueOf(profileData.getTotalRecipes()));
+            }
+        });
+
+        profileViewModel.getUserRecipes().observe(getViewLifecycleOwner(), recipes -> {
+            if (recipes != null) {
+                adapter.setRecipeList(recipes);
+            }
+        });
+    }
+
+
+    private void openSaveRecipesFragment() {
+        NavHostFragment.findNavController(this)
+                .navigate(R.id.action_profileFragment_to_saveRecipesFragment);
     }
 
     private void openProfileSettingsFragment() {
         NavHostFragment.findNavController(this)
                 .navigate(R.id.action_profileFragment_to_settingsProfileFragment);
     }
+
     private void openSettingsFragment() {
         NavHostFragment.findNavController(this)
                 .navigate(R.id.action_profileFragment_to_settingsFragment);
